7a9cbaa60cef36dbd299e22ff070ffff8281aedb
Medvezo <mansur.mustafayev@gmail.com>
2023-11-09 17:23:51 +0400

    Refactor Hero comp, add adv motions in /utils/motion.ts

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -1,15 +1,38 @@
+"use client";
 import Image from "next/image";
+import { m, LazyMotion, domAnimation } from "framer-motion";
+import { slideInFromLeft, slideInFromRight, fadeIn } from "@/utils/motion";
+
 export default function Hero() {
 	return (
-		<section className="flex flex-col lg:flex-row items-center justify-between gap-10 w-full h-full m-auto">
-            <p>Explore Games</p>
-			<Image
-				src="/Hero.png"
-				alt="Hero section react arcadeillustration"
-				width={600}
-				height={300}
-                className="rounded-3xl "
-			/>
-		</section>
+		<LazyMotion features={domAnimation}>
+			<m.section
+				initial="hidden"
+				animate="visible"
+				className="flex flex-col lg:flex-row items-center justify-between gap-10 w-full h-full m-auto pb-16">
+				<div className="flex flex-col gap-6 mt-6 text-5xl md:text-6xl text-white max-w-[600px] w-auto h-auto">
+					<m.span variants={slideInFromLeft(0.5)}>
+						Explore and play
+						<p className="text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-green-600">
+							{" "}
+							React Games{" "}
+						</p>
+					</m.span>
+				</div>
+				<m.div
+					variants={slideInFromRight(0.5)}
+					className="bg-violet-500/10 rounded-3xl">
+					<m.div variants={fadeIn(1.2)} className="backdrop-blur-lg rounded-3xl ">
+						<Image
+							src="/Hero.png"
+							alt="Hero section react arcadeillustration"
+							width={600}
+							height={300}
+							className="rounded-3xl  "
+						/>
+					</m.div>
+				</m.div>
+			</m.section>
+		</LazyMotion>
 	);
 }
---
+++ src/utils/motion.ts
@@ -0,0 +1,52 @@
+export const slideInFromTop = {
+	hidden: { y: -100, opacity: 0 },
+	visible: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			delay: 0.5,
+			duration: 0.5,
+		},
+	},
+};
+
+export function slideInFromLeft(delay: number) {
+	return {
+		hidden: { x: -100, opacity: 0 },
+		visible: {
+			x: 0,
+			opacity: 1,
+			transition: {
+				delay: delay,
+				duration: 0.5,
+			},
+		},
+	};
+}
+
+export function slideInFromRight(delay: number) {
+	return {
+		hidden: { x: 100, opacity: 0 },
+		visible: {
+			x: 0,
+			opacity: 1,
+			transition: {
+				delay: delay,
+				duration: 0.5,
+			},
+		},
+	};
+}
+
+export function fadeIn(delay: number) {
+	return {
+		hidden: { opacity: 0 },
+		visible: {
+			opacity: 1,
+			transition: {
+				delay: delay,
+				duration: 0.7,
+			},
+		},
+	};
+}
