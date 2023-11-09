dda55b8a955c0e03db963ae397f258dbe793c63f
Medvezo <mansur.mustafayev@gmail.com>
2023-11-09 14:49:23 +0400

    Add Navbar comp, add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'React Arcade Zone',
-  description: 'React Arcade Zone is games hub built by React and Next JS 14.',
-}
+	title: "React Arcade Zone",
+	description: "React Arcade Zone is games hub built by React and Next JS 14.",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
 }
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+
+export default function Navbar() {
+	return (
+		<div className="w-full h-20 fixed top-0 shadow-lg shadow-violet-950/50 bg-white/10 backdrop-blur-md z-50 lg:px-10">
+			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden">
+				<Link
+					href="/"
+					className="h-auto w-auto flex flex-row items-center justify-center gap-3">
+					<Image
+						src="/Logo.png"
+						alt="logo"
+						width={80}
+						height={80}
+						className="cursor-pointer hover:animate-spin-slow "
+					/>
+
+					<span className="font-bold text-xl lg:text-2xl text-transparent  bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
+						React Arcade Zone
+					</span>
+				</Link>
+
+				<div className="flex flex-row gap-5">
+					<a
+						href="https://github.com/Medvezo/react-arcade-zone"
+						target="_blank"
+						rel="noopener noreferrer">
+						<BsGithub className="w-10 h-10" />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
