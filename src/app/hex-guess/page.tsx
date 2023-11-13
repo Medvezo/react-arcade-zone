1f38671e46d27ce66be775760847c67bc5daa387
Medvezo <mansur.mustafayev@gmail.com>
2023-11-14 00:22:16 +0400

    Add ready generateHex func

--- src/app/hex-guess/page.tsx
+++ src/app/hex-guess/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const hexSymbols = [
+	"0",
 	"1",
 	"2",
 	"3",
@@ -19,18 +20,34 @@ const hexSymbols = [
 	"F",
 ];
 
-function generateHex() {}
+function generateHex() {
+	let hex = "#";
+	for (let num = 0; num < 6; num++) {
+		hex += hexSymbols[Math.floor(Math.random() * 16)];
+	}
+	return hex;
+}
 
 export default function Home() {
 	const [hex, setHex] = useState<string>("#702256");
+    useEffect(() => {
+        console.log(hex)
+    }, [hex])
 
 	return (
-		<main className="pt-20 flex justify-center items-center min-h-screen">
-			<div
-				className={` w-96 h-96 block `}
-				style={{ backgroundColor: hex }}>
-				{" "}
-			</div>
+		<main className="pt-20 flex flex-col justify-around items-center min-h-screen">
+			<h1 className="text-5xl">Hex Guess</h1>
+			<section>
+				<div className={` w-96 h-96 block `} style={{ backgroundColor: hex }}>
+					{" "}
+				</div>
+				<button
+					onClick={() => setHex((hex) => (hex = generateHex()))}
+					className="text-white">
+					{" "}
+					Generate
+				</button>
+			</section>
 		</main>
 	);
 }
