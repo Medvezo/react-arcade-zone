a6b3c030d5b4f25afb06fae81b01a543f014cde0
Medvezo <mansur.mustafayev@gmail.com>
2023-11-28 23:20:59 +0400

    Add Balloon Provider

---
+++ src/components/providers/BalloonProvider.tsx
@@ -0,0 +1,25 @@
+import { createContext, useReducer } from "react";
+import { initialState, reducer } from "./balloon-store";
+
+type TBalloonContext = {
+	state: typeof initialState;
+	dispatch: React.Dispatch<any>;
+};
+
+const BalloonContext = createContext<TBalloonContext>({
+	state: initialState,
+	dispatch: () => null,
+});
+
+export default function BalloonProvider({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	return (
+		<BalloonContext.Provider value={{ state, dispatch }}>
+			{children}
+		</BalloonContext.Provider>
+	);
+}
