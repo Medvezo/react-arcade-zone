bac9aa225d803378c641a1341576eab6e15c1a9a
Medvezo <mansur.mustafayev@gmail.com>
2023-12-22 22:49:21 +0400

    feat: add reveleadGrid state for onClicks

--- src/app/pair-match/page.tsx
+++ src/app/pair-match/page.tsx
@@ -6,12 +6,18 @@ import { pairs } from "@/lib/const";
 
 export default function Home() {
 	const pairsShuffled = pairs.sort(() => Math.random() - 0.5); // Shuffle the array
+	// Display grid of 4x4 cards
 	const [grid, setGrid] = useState([
 		[pairsShuffled[5], pairsShuffled[6], pairsShuffled[1], pairsShuffled[2]],
 		[pairsShuffled[2], pairsShuffled[7], pairsShuffled[0], pairsShuffled[7]],
 		[pairsShuffled[1], pairsShuffled[3], pairsShuffled[4], pairsShuffled[5]],
 		[pairsShuffled[4], pairsShuffled[3], pairsShuffled[6], pairsShuffled[0]],
 	]);
+	// Revealed grid of 4x4 cards on click
+	const [revealedGrid, setRevealedGrid] = useState(
+		new Array(grid.length).fill(new Array(grid[0].length).fill(false))
+	);
+    console.log(revealedGrid);
 
 	return (
 		<>
@@ -22,8 +28,8 @@ export default function Home() {
 				<div className="flex flex-col gap-3 lg:gap-5 px-3 lg:px-5">
 					{grid.map((row, rowIdx) => (
 						<div key={rowIdx} className="flex gap-5">
-							{row.map((pair, idx) => (
-								<PairCard key={idx} pair={pair} />
+							{row.map((pair, colIdx) => (
+								<PairCard key={colIdx} pair={pair} />
 							))}
 						</div>
 					))}
