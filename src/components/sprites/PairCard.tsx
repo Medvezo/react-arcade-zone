fc5cd740740faaa5846bb177c692cc0b390139dd
Medvezo <mansur.mustafayev@gmail.com>
2023-12-23 15:15:26 +0400

    feat: add flipping  on click

--- src/components/sprites/PairCard.tsx
+++ src/components/sprites/PairCard.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import { Card, CardBody, Image } from "@nextui-org/react";
 import NextImage from "next/image";
-import { TPair } from "@/lib/types";
+import { TPairCard } from "@/lib/types";
+import { useEffect, useState } from "react";
 
-export default function PairCard({ pair }: { pair: TPair }) {
+export default function PairCard({
+	pair,
+	handleClick,
+	rowIdx,
+	colIdx,
+	revealedGrid,
+}: TPairCard) {
+	const [flipped, setFlipped] = useState<boolean>(false);
+	useEffect(() => {
+		if (revealedGrid[rowIdx][colIdx]) {
+			setFlipped(true);
+		} else {
+			setFlipped(false);
+		}
+	}, [revealedGrid, rowIdx, colIdx]);
 	return (
-		<Card shadow="sm" isPressable>
+		<Card shadow="sm" isPressable onClick={() => handleClick(rowIdx, colIdx)}>
 			<CardBody>
 				<Image
+					className="lg:w-24 lg:h-24"
 					as={NextImage}
-					src={pair.src}
+					src={flipped ? pair.src : "pair-match/react.svg"}
 					width="70"
 					height="70"
 					alt={`${pair.title} logo`}
