324b6703dc42df5711f0d1c467cdc620f7ef4947
Medvezo <mansur.mustafayev@gmail.com>
2023-12-23 04:12:15 +0400

    feat: add TPairCard type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,13 @@
+//! Match Pair Game 
 export type TPair = {
 	title: string;
 	src: string;
+};
+
+export type TPairCard = {
+	pair: TPair;
+	handleClick: (rowIdx: number, colIdx: number) => void;
+	rowIdx: number;
+	colIdx: number;
+	revealedGrid: boolean[][];
 };
