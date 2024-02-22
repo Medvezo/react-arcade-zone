085f75dfbd4b153f4a6a6a75d982796008b42093
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-02-22 23:47:50 +0400

    feat: add conditional rendering for TTTCell comp

--- src/components/sprites/TTTCell.tsx
+++ src/components/sprites/TTTCell.tsx
@@ -1,5 +1,20 @@
-export default function TTTCell() {
-  return (
-    <div>X</div>
-  )
+type CellType = {
+	value: number;
+};
+
+export default function TTTCell({ value }: CellType) {
+
+    // Condition for cell render
+	if (value > 0){
+        return (
+            <div className="text-blue-500 text-xl">X</div>
+        )
+    } 
+	else if (value < 0){
+        return (
+            <div className="text-red-500 text-xl w-20 h-20 aspect-square">O</div>
+        )
+    }
+
+	return <div className="w-20 h-20">{" "}</div>;
 }
