9efb14296128a034b7a22b6bcdff28b8e15e4129
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-02-23 01:56:17 +0400

    fix: making TTT grid responsive

--- src/app/tic-tac-toe/page.tsx
+++ src/app/tic-tac-toe/page.tsx
@@ -3,9 +3,9 @@ export default function page() {
 	return (
 		<>
 			{/* Background image */}
-			<div className="fixed w-full h-screen bg-synth bg-cover blur-2xl bg-center bg-no-repeat -z-10" />
+			<div className="fixed w-full h-screen bg-synth bg-cover blur-3xl bg-center bg-no-repeat -z-10" />
 
-			<main className="flex justify-center items-center h-screen flex-col">
+			<main className="flex justify-center items-center h-screen flex-col gap-10">
 				<TicTacToeGrid gridSize={3}/>
 			</main>
 		</>
--- src/components/container/TicTacToeGrid.tsx
+++ src/components/container/TicTacToeGrid.tsx
@@ -7,20 +7,24 @@ type TTicTacToeGrid = {
 };
 
 export default function TicTacToeGrid({ gridSize }: TTicTacToeGrid) {
-	const grid = new Array(gridSize).fill(new Array(gridSize).fill(0));
+	const grid = new Array(gridSize).fill(new Array(gridSize).fill(-1));
 	useEffect(() => {
 		console.log(grid);
 	}, [grid]);
 
 	return (
 		<>
-			<div className="">Tic Tac Toe</div>
+			<div className="text-3xl">Tic Tac Toe</div>
 
-			<div className={` grid grid-cols-${gridSize} divide-x-2 divide-white `}>
+			<div
+				className={` grid grid-cols-${gridSize} w-[400px] sm:w-[500px] divide-x-2 divide-white bg-teal-700 bg-opacity-30 p-5 rounded-3xl`}
+			>
 				{grid.map((row, rowIdx) => (
-					<div key={rowIdx} className="divide-y-2  divide-white">
+					<div key={rowIdx} className="divide-y-2 divide-white ">
 						{row.map((cell: number, colIdx: number) => (
-							<TTTCell key={`${rowIdx} ${colIdx}`} />
+							<div key={`${rowIdx} ${colIdx}`} className={`aspect-square `}>
+								<TTTCell value={cell} />
+							</div>
 						))}
 					</div>
 				))}
