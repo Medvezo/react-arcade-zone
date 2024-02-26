c015e6ab23264188c2377382c053d41bde14eb9f
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-02-26 15:40:08 +0400

    refactor: minor improvement for UI/UX

--- src/components/layout/TTTHeader.tsx
+++ src/components/layout/TTTHeader.tsx
@@ -25,29 +25,31 @@ export default function TTTHeader({ onRestart }: TTTHeader) {
 				Tic Tac Toe
 			</h2>
 
-			{state.isGameOver && (
+			{state.isGameOver && (	// Grid size selection
 				<Slider
-					size="sm"
+					size="md"
+					showTooltip
 					step={1}
-					color="foreground"
+					color="primary"
 					label="Grid Size"
 					showSteps={true}
 					maxValue={10}
 					minValue={3}
 					defaultValue={3}
+					getValue={(size) => `${size}x${size}`}
 					className="max-w-md"
 				/>
 			)}
-
-			{state.isGameOver ? (
-				state.winner && (
+	
+			{state.isGameOver ? ( 	// whether is first game or not
+				state.winner && (	// checking if null
 					<p
 						className={`text-2xl text-green-500 flex max-h-16 justify-center items-center  ${pixelFont}`}
 					>
 						Winner: <TTTCell value={state.winner} />
 					</p>
 				)
-			) : (
+			) : (					// showing next move
 				<p className="text-2xl lg:text-3xl font-bold text-cyan-50 z-40 flex justify-center items-center gap-3">
 					Next move:{" "}
 					<span className="w-10 h-10 ">
@@ -56,10 +58,10 @@ export default function TTTHeader({ onRestart }: TTTHeader) {
 				</p>
 			)}
 
-			{state.isGameOver && (
+			{state.isGameOver && (	// Play game 
 				<Button
 					variant="ghost"
-					color="secondary"
+					color="primary"
 					onClick={onRestart}
 					className="text-white text-xl"
 					size="lg"
